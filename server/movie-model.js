81133a90f63c7c7d9fb24e8555c6f6a00d5e79f7
marc <marc.trebes@gmail.com>
2026-04-26 19:11:24 +0200

    Styling and Header Background

--- server/movie-model.js
+++ server/movie-model.js
@@ -1,4 +1,49 @@
 module.exports = {
+    tt8367814: {
+    imdbID: "tt8367814",
+    Title: "The Gentlemen",
+    Released: "2020-01-24",
+    Runtime: 113,
+    Genres: ["Action", "Crime", "Comedy"],
+    Directors: ["Guy Ritchie"],
+    Writers: ["Guy Ritchie", "Ivan Atkinson", "Marn Davies"],
+    Actors: ["Matthew McConaughey", "Charlie Hunnam", "Michelle Dockery"],
+    Plot: "An American expat tries to sell off his highly profitable marijuana empire in London, triggering plots, schemes, bribery and blackmail in an attempt to steal his domain out from under him.",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjE2ZjQ4ZGMtZjFhMi00NmI5LTliNjEtODczMWMxNjliZjgxXkEyXkFqcGc@._V1_SX1200.jpg",
+    Metascore: 51,
+    imdbRating: 7.8
+  },
+
+  tt0258463: {
+    imdbID: "tt0258463",
+    Title: "The Bourne Identity",
+    Released: "2002-06-14",
+    Runtime: 119,
+    Genres: ["Action", "Mystery", "Thriller"],
+    Directors: ["Doug Liman"],
+    Writers: ["Tony Gilroy", "W. Blake Herron", "Robert Ludlum"],
+    Actors: ["Matt Damon", "Franka Potente", "Chris Cooper"],
+    Plot: "A man is picked up by a fishing boat, bullet-riddled and suffering from amnesia, before racing to elude assassins and regain his memory.",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYTk1ZTcyMWMtMWUxYS00MmEzLTlmODYtOTk1MGRjOTg1ZjlmXkEyXkFqcGc@._V1_SX1200.jpg",
+    Metascore: 68,
+    imdbRating: 7.9
+  },
+
+  tt0816692: {
+    imdbID: "tt0816692",
+    Title: "Interstellar",
+    Released: "2014-11-07",
+    Runtime: 169,
+    Genres: ["Adventure", "Drama", "Sci-Fi"],
+    Directors: ["Christopher Nolan"],
+    Writers: ["Jonathan Nolan", "Christopher Nolan"],
+    Actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+    Plot: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_SX1200.jpg",
+    Metascore: 74,
+    imdbRating: 8.7
+  },
+
   tt0084787: {
     imdbID: "tt0084787",
     Title: "The Thing",
@@ -14,6 +59,7 @@ module.exports = {
     Metascore: 57,
     imdbRating: 8.2,
   },
+
   tt0120737: {
     imdbID: "tt0120737",
     Title: "The Lord of the Rings: The Fellowship of the Ring",
@@ -29,6 +75,7 @@ module.exports = {
     Metascore: 92,
     imdbRating: 8.8,
   },
+
   tt0085959: {
     imdbID: "tt0085959",
     Title: "The Meaning of Life",
@@ -44,6 +91,7 @@ module.exports = {
     Metascore: 72,
     imdbRating: 7.5,
   },
+
   tt0107048: {
     imdbID: "tt0107048",
     Title: "Groundhog Day",
@@ -59,6 +107,7 @@ module.exports = {
     Metascore: 72,
     imdbRating: 8.1,
   },
+
   tt0124315: {
     imdbID: "tt0124315",
     Title: "The Cider House Rules",
@@ -74,6 +123,7 @@ module.exports = {
     Metascore: 75,
     imdbRating: 7.4,
   },
+  
   tt0248808: {
     imdbID: "tt0248808",
     Title: "For the Birds",
